bf8a14dcefd97916109eb6fbfb8826ee552ce769
karlaemartins <karlaemartinss@gmail.com>
2025-01-11 14:58:41 -0300

    Initial Commit

---
+++ RickyAndMortyApp/RickyAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickyAndMortyAppApp.swift
+//  RickyAndMortyApp
+//
+//  Created by Karla E. Martins Fernandes on 11/01/25.
+//
+
+import SwiftUI
+
+@main
+struct RickyAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
